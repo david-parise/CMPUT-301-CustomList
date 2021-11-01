80b027b3c0559e4af6e3cd45a6b9de6d07a86bf6
David Parise <parise@ualberta.ca>
2021-11-01 13:29:59 -0600

    Push with addCity implementation

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -59,7 +59,6 @@ public class CustomList extends ArrayAdapter<City> {
      * @param city
      */
     public void addCity(City city) {
-        // cities.add(city)
-        return;
+        cities.add(city);
     }
 }
